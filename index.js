285f32a39ea0fc83bfb459a8f9362b8808d6cc00
semeikaya <106886023+semeikaya@users.noreply.github.com>
2022-09-09 15:21:34 +0300

    Завершил бэкенд новостного сайта

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+app.use(require("./routes/news.route.js"));
+app.use(require("./routes/comments.route.js"));
+app.use(require("./routes/categories.route.js"));
+
 mongoose.connect("mongodb+srv://bersyak:web2000web@cluster0.akjwcf0.mongodb.net/news", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('Успешно соединились с сервером MongoDB'))
     .catch(() => console.log('Ошибка при соединении с сервером MongoDB'))
 
-app.use(require("./routes/news.route.js"));
-app.use(require("./routes/comments.route.js"));
-app.use(require("./routes/categories.route.js"));
+
 app.listen(4000, () => {
     console.log('Сервер успешно запущен');
 });
